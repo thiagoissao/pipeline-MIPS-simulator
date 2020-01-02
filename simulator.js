c9b271f51852ca3a51ebc399054a3f6bfe2b30ce
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-01-02 11:16:54 -0300

    Implementa console logs e memoria de dados

--- simulator.js
+++ simulator.js
@@ -4,6 +4,7 @@ const FILEPATH = './tests/data_ok.dat'
 
 //Leitura do arquivo é armazenado em um array na qual cada linha representa um espaço no vetor
 let instructionsMemory
+let dataMemory = []
 
 //Registradores
 let $r1, $r2, $r3, $r4, $r5, $r6, $r7, $r8, $r9, $r10
@@ -30,7 +31,42 @@ const initialize = () => {
   }
   $r1 = $r2 = $r3 = $r4 = $r5 = $r6 = $r7 = $r8 = $r9 = $r10 = 0
   PC = 0
+  for (let i = 0; i < 20; i++) {
+    dataMemory[i] = { pos: i, value: 0 }
+  }
 }
+
+const printDataMemory = () => {
+  console.log('--------------Memória de Dados--------------')
+  console.log('Posição\t\tValor')
+  dataMemory.forEach(v => {
+    console.log(v.pos + '\t\t' + v.value)
+  })
+  console.log('------------------------------------------')
+}
+
+const printRegisters = () => {
+  console.log('--------------Registradores--------------')
+  console.log('Registrador\tValor')
+  console.log('$r1\t\t' + $r1)
+  console.log('$r2\t\t' + $r2)
+  console.log('$r3\t\t' + $r3)
+  console.log('$r4\t\t' + $r4)
+  console.log('$r5\t\t' + $r5)
+  console.log('$r6\t\t' + $r6)
+  console.log('$r7\t\t' + $r7)
+  console.log('$r8\t\t' + $r8)
+  console.log('$r9\t\t' + $r9)
+  console.log('$r10\t\t' + $r10)
+  console.log('------------------------------------------')
+}
+
+const printPC = () => {
+  console.log('PC\t\t' + PC)
+  console.log()
+}
+
+const printPipeline = () => { }
 
 const lw = (reg, adress) => { }
 const sw = (reg, address) => { }
@@ -49,4 +85,6 @@ const execute = () => { }
 const write = () => { }
 
 initialize()
-console.log(PC)
+printDataMemory()
+printRegisters()
+printPC()
